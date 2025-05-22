d8011963bb4f1e0a468eeee52fd8c1c272b55511
Akash1247 <ay99059@gmail.com>
2025-05-22 23:25:39 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.payload;
+
+import com.ecommerce.project.model.Category;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialprice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
---
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+
+public interface ProductService {
+    ProductDTO addProuct(Long categoryId, ProductDTO product);
+
+    ProductResponse getAllProducts();
+
+    ProductResponse searchByCategory(Long categoryId);
+
+    ProductResponse searchProductByKeyword(String keyword);
+
+    ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProduct(ProductDTO productDTO, Long productID);
+}
