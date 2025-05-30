be92fc8e24a8d76093799799edffe9a4f9784059
Akash1247 <ay99059@gmail.com>
2025-05-30 08:09:21 +0530

    Added User roles

--- src/main/java/com/ecommerce/project/Attirely.java
+++ src/main/java/com/ecommerce/project/Attirely.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbEcomApplication {
+public class Attirely {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbEcomApplication.class, args);
+		SpringApplication.run(Attirely.class, args);
 	}
 
 }
